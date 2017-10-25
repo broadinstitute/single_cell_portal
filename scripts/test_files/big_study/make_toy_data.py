eab798c066a21f5fbc8e9d6b4e99d1638b0aa5f7
Eric Weitz <eweitz@broadinstitute.org>
2017-10-25 14:36:53 -0400

    Enable output of any number of files, leverage multiprocessing

--- scripts/test_files/big_study/make_toy_data.py
+++ scripts/test_files/big_study/make_toy_data.py
@@ -8,7 +8,28 @@ python make_toy_data.py
 """
 
 from random import randrange, uniform
+import argparse
+import multiprocessing
 
+args = argparse.ArgumentParser(
+    prog="make_toy_data.py",
+    description=(
+        "Generate data to simulate large study, e.g. to test download " +
+        "features."
+    ),
+    formatter_class=argparse.HelpFormatter
+)
+args.add_argument(
+    "--num_files", default=3, type=int, dest="num_files",
+    help="Number of toy data files output",
+)
+args.add_argument(
+    "--num_cores", default=None, type=int, dest="num_cores",
+    help="Number of CPUs to use",
+)
+parsed_args = args.parse_args()
+num_files = parsed_args.num_files
+num_cores = parsed_args.num_cores
 
 def get_signature_content(prefix):
     """
@@ -23,11 +44,12 @@ def get_signature_content(prefix):
 
     letters = ['A', 'B', 'C', 'D']
 
-    num_items = 16584
+    num_columns = 16584
+    num_rows = 80
 
+    # Generate header
     header = "GENE\t"
-
-    for i in range(1, num_items):
+    for i in range(1, num_columns):
         random_string = ''
         for j in range(1, 16):
             # Generate a 16-character string of random combinations of
@@ -42,10 +64,10 @@ def get_signature_content(prefix):
             ri3 + random_string + '-1\t'
         )
 
+    # Generate values below header
     values = ''
-    for i in range(1, 80):
-        print('On item ' + str(i))
-        for j in range(1, num_items):
+    for i in range(1, num_rows):
+        for j in range(1, num_columns):
             # Random number between 0 and -0.099999999999999
             random_small_float = uniform(0, 0.1) * -1
             values += str(random_small_float) + '\t'
@@ -53,9 +75,32 @@ def get_signature_content(prefix):
     signature_data = header + '\n' + values
     return signature_data
 
-# Produce three text files, each ~25 MB
-prefixes = ['AB', 'CD', 'EF']
-for prefix in prefixes:
+
+def pool_processing(prefix):
+    """ Function called by each CPU core in our pool of available CPUs
+    """
     content = get_signature_content(prefix)
-    with open(prefix + '_toy_data_signature_50000.txt', 'w') as f:
+    file_name = prefix + '_toy_data_signature_50000.txt'
+    with open(file_name, 'w') as f:
         f.write(content)
+        print('Wrote ' + file_name)
+
+prefixes = []
+
+# Produce a number of text files, each ~25 MB
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+for i in range(0, num_files):
+    index = i*2
+    prefix = alphabet[index:index+2] # e.g. 'AB' or 'CD'
+    prefixes.append(prefix)
+
+if num_cores is None:
+    num_cores = multiprocessing.cpu_count()
+    if num_cores > 1:
+        # Use all cores except 1 in machines with multiple CPUs
+        num_cores -= 1
+
+pool = multiprocessing.Pool(num_cores)
+
+# Distribute calls to get_signature_content to multiple CPUs
+pool.map(pool_processing, prefixes)
