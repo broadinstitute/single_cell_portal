401bfcae1c3a05e2481b6ee84258947ab5def781
Eric Weitz <eweitz@broadinstitute.org>
2018-10-11 07:11:35 -0400

    Get annotation metadata from Ensembl API, draft gzip handling

--- scripts/genomes/parse_genome_annotations.py
+++ scripts/genomes/parse_genome_annotations.py
@@ -1,6 +1,64 @@
 """Parse genome annotations, output position-sorted GTF in plaintext and compressed
 """
 
+import json
+import urllib.request as request
+
 from utils import *
 
-species_list = get_species_list(organisms_path)
+scp_species = get_species_list('organisms.tsv')
+
+def get_ensembl_metadata():
+    """Get organism, assembly, and annotation release metadata from Ensembl
+    """
+    ensembl_metadata = {}
+
+    # API docs: https://rest.ensembl.org/documentation/info/species
+    url = 'https://rest.ensembl.org/info/species?content-type=application/json'
+    with request.urlopen(url) as response:
+        data = response.read().decode('utf-8')
+    ensembl_species = json.loads(data)['species']
+
+    for species in ensembl_species:
+        taxid = species['taxon_id']
+        releases_by_taxid[taxid] = {
+            'organism': species['name'],
+            'taxid': species['taxid'],
+            'assembly_name': species['assembly'],
+            'assembly_accession': species['accession'],
+            'release': species['release']
+        }
+
+    return ensembl_metadata
+
+def get_ensembl_gtf_url(organism_metadata):
+    """ Construct the URL of an Ensembl genome annotation GTF file.
+
+    Example URL:
+    http://ftp.ensembl.org/pub/release-94/gtf/homo_sapiens/Homo_sapiens.GRCh38.94.gtf.gz
+    """
+
+    release = organism_metadata['release']
+    organism = organism_metadata['organism']
+    organism_upper = organism[0].upper() + organism[1:]
+
+    origin = 'http://ftp.ensembl.org'
+    dir = '/pub/release' + release + '/gtf/' + organism + '/'
+    filename = organism_upper + '.' + assembly_name + 'gtf.gz'
+
+    gtf_url = origin + dir + filename
+    return gtf_url
+
+
+def get_ensembl_gtf(organism_metadata):
+    gtf_url = get_ensembl_gtf_url(organism_metadata)
+
+    return
+
+
+ensembl_metadata = get_ensembl_metadata()
+
+for species in scp_species:
+    taxid = species[2]
+    organism_metadata = ensembl_metadata[taxid]
+    gtf = get_ensembl_gtf(organism_metadata)
--- scripts/genomes/utils.py
+++ scripts/genomes/utils.py
@@ -1,3 +1,7 @@
+"""Common functions for genome assembly and annotation data processing.
+"""
+
+import gzip
 import os
 import urllib.request as request
 
@@ -5,12 +9,37 @@ def get_species_list(organisms_path):
     """Get priority species to support in Single Cell Portal
     """
     with open(organisms_path) as f:
-        species_list = [line.split('\t') for line in f.readlines()[1:]]
+        species_list = [line.strip().split('\t') for line in f.readlines()[1:]]
     return species_list
+
+def get_gzipped_content(output_path, content_url):
+    """Fetch remote gzipped content, or read it from disk if cached
+    """
+    if os.path.exists(output_path):
+        # Use locally cached content if available
+        with open(output_path, 'rb') as f:
+            content = gzip.GzipFile(fileobj=f).readlines()
+    else:
+        # If local report absent, fetch remote content and cache it
+        request_obj = request.Request(
+            content_url,
+            headers={"Accept-Encoding": "gzip"}
+        )
+        with request.urlopen(request_obj) as response:
+            remote_content = gzip.GzipFile(fileobj=response)
+            remote_content.read()
+            print(remote_content)
+            with open(output_path, 'wb') as f:
+                f.write(remote_content)
+            content = remote_content
+    return content
 
 def get_content(output_path, content_url):
     """Fetch remote content, or read it from disk if cached
     """
+    if content_url[-3:] == '.gz':
+        return get_gzipped_content(output_path, content_url)
+
     if os.path.exists(output_path):
         # Use locally cached content if available
         with open(output_path) as f:
