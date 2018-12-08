6e2418c86268977f1379d9046690c6a114c0a49e
TimothyTickle <timothyltickle@gmail.com>
2018-12-08 11:07:51 -0500

    Added in permission shares. Moved deleting to access 'Remove' using the same commandline tool.

--- scripts/SCPAPI.py
+++ scripts/SCPAPI.py
@@ -14,11 +14,18 @@ c_SUCCESS_RET_KEY = "success"
 ## Standard Error Codes
 c_STUDY_EXISTS = 101
 c_STUDY_EXISTS_TEXT = "Can not create a study with a name that is in use."
+c_STUDY_DOES_NOT_EXIST = 102
+c_STUDY_DOES_NOT_EXIST_TEXT = "The study does not exist, please create first."
+c_INVALID_SHARE_MODE = 103
+c_INVALID_SHARE_MODE_TEXT = "The access you want to give the user is not an access I understand. Please check."
+c_INVALID_SHARE_MISSING = 104
+c_INVALID_SHARE_MISSING_TEXT = "Can not remove the share, it does not exist."
 c_NO_ERROR = 0
 c_NO_ERROR_TEXT = "No error occurred."
 
 ## API Error codes
 c_API_OK = 200
+c_DELETE_OK = 204
 c_API_AUTH = 401
 c_API_AUTH_TEXT = "User is not authenticated"
 c_API_AUTH_STUDY = 403
@@ -32,6 +39,14 @@ c_API_INVALID_STUDY_TEXT = "Study validation failed"
 c_API_BACKEND_ERROR = 500
 c_API_BACKEND_ERROR_TEXT = "Server error when attempting to synchronize FireCloud workspace or access GCS objects"
 
+# Share access modes
+c_ACCESS_EDIT = "Edit"
+c_ACCESS_REVIEWER = "Reviewer"
+c_ACCESS_VIEW = "View"
+c_ACCESS_REMOVE = "Remove"
+c_PERMISSIONS = [c_ACCESS_EDIT, c_ACCESS_REVIEWER, c_ACCESS_VIEW, c_ACCESS_REMOVE]
+
+
 # The expected header
 class APIManager:
 
@@ -39,12 +54,14 @@ class APIManager:
         print("INIT")
         self.api = "https://portals.broadinstitute.org/single_cell/api/v1/"
         self.studies = None
+        self.name_to_id = None
 
     def login(self, token=None, test=False):
         """
         :param token: User token to use with API
         :return: Boolean Indicator of success or failure (False)
         """
+        ## TODO add in auth from a file.
         print("LOGIN")
         if token is None:
             token = APIManager.do_browser_login(test=test)
@@ -65,8 +82,7 @@ class APIManager:
         print(command)
         if test:
             return({c_SUCCESS_RET_KEY: True,c_CODE_RET_KEY: c_API_OK})
-        head = {c_AUTH:'token {}'.format(self.token),
-                'Accept':'application/json'}
+        head = {c_AUTH:'token {}'.format(self.token), 'Accept':'application/json'}
         return(APIManager.check_api_return(requests.get(command, headers=head)))
 
     def do_post(self, command, values, test=False):
@@ -75,15 +91,35 @@ class APIManager:
         print(values)
         if test:
             return({c_SUCCESS_RET_KEY: True,c_CODE_RET_KEY: c_API_OK})
-        head = {c_AUTH:'token {}'.format(self.token),
-                'Accept':'application/json'}
+        head = {c_AUTH: 'token {}'.format(self.token), 'Accept': 'application/json'}
         return(APIManager.check_api_return(requests.post(command, headers=head, json=values)))
+
+    def do_patch(self, command, values, test=False):
+        print("DO PATCH")
+        print(command)
+        print(values)
+        if test:
+            return({c_SUCCESS_RET_KEY: True,c_CODE_RET_KEY: c_API_OK})
+        head = {c_AUTH: 'token {}'.format(self.token), 'Accept': 'application/json'}
+        return(APIManager.check_api_return(requests.patch(command, headers=head, json=values)))
+
+    def do_delete(self, command, test=False):
+        print("DO DELETE")
+        print(command)
+        if test:
+            return({c_SUCCESS_RET_KEY: True,c_CODE_RET_KEY: c_DELETE_OK})
+        head = {c_AUTH: 'token {}'.format(self.token), 'Accept': 'application/json'}
+        return(APIManager.check_api_return(requests.delete(command, headers=head)))
 
     def describe_error_code(error_code):
         ret_error_codes = {
             c_STUDY_EXISTS:c_STUDY_EXISTS_TEXT,
+            c_STUDY_DOES_NOT_EXIST:c_STUDY_DOES_NOT_EXIST_TEXT,
+            c_INVALID_SHARE_MODE:c_INVALID_SHARE_MODE_TEXT,
+            c_INVALID_SHARE_MISSING:c_INVALID_SHARE_MISSING_TEXT,
             c_NO_ERROR:c_NO_ERROR_TEXT,
             c_API_OK:c_NO_ERROR_TEXT,
+            c_DELETE_OK:c_NO_ERROR_TEXT,
             c_API_AUTH:c_API_AUTH_TEXT,
             c_API_AUTH_STUDY:c_API_AUTH_STUDY_TEXT,
             c_API_UNKNOWN_STUDY:c_API_UNKNOWN_STUDY_TEXT,
@@ -95,7 +131,7 @@ class APIManager:
 
     def check_api_return(ret):
         api_return = {}
-        api_return[c_SUCCESS_RET_KEY] = ret.status_code == c_API_OK
+        api_return[c_SUCCESS_RET_KEY] = ret.status_code in [c_API_OK, c_DELETE_OK]
         api_return[c_CODE_RET_KEY] = ret.status_code
         api_return[c_RESPONSE] = ret
         return(api_return)
@@ -107,7 +143,9 @@ class APIManager:
             resp[c_STUDIES_RET_KEY] = ["TESTING 1","TESTING 2"]
         else:
             if(resp[c_SUCCESS_RET_KEY]):
-                self.studies = [str(element.get('name')) for element in resp[c_RESPONSE].json()]
+                self.studies = [str(element['name']) for element in resp[c_RESPONSE].json()]
+                self.name_to_id = [[str(element['name']), str(element['_id']['$oid'])] for element in resp[c_RESPONSE].json()]
+                self.name_to_id = {key: value for (key,value) in self.name_to_id}
                 resp[c_STUDIES_RET_KEY] = self.studies
         return(resp)
 
@@ -117,11 +155,7 @@ class APIManager:
                      test=False):
         print("CREATE STUDY:: " + studyName)
         # If we don't know what studies they have get them so we do not create a study already existing.
-        if self.studies is None:
-            ret = self.get_studies()
-            if not ret[c_SUCCESS_RET_KEY]:
-                return(ret)
-        if studyName in self.studies:
+        if self.study_exists(studyName):
             return ({
                 c_SUCCESS_RET_KEY: False,
                 c_CODE_RET_KEY: c_STUDY_EXISTS
@@ -130,7 +164,77 @@ class APIManager:
                      "description":studyDescription,
                      "public":isPublic}
         resp = self.do_post(command=self.api + "studies", values=studyData, test=test)
-        if resp[c_SUCCESS_RET_KEY]:
-            self.studies.append(studyName)
+        if resp[c_SUCCESS_RET_KEY] and not test:
+            self.get_studies()
         return(resp)
 
+    def set_permission(self, study_name, email, access, deliver_email=False, test=False):
+        print("SET PERMISSION: "+" ".join(str(i) for i in [study_name, email, access]))
+        # Make sure the access value is valid
+        if not access in c_PERMISSIONS:
+            return {
+                c_SUCCESS_RET_KEY: False,
+                c_CODE_RET_KEY: c_INVALID_SHARE_MODE
+            }
+        # Error if the study does not exist
+        if not self.study_exists(study_name) and not test:
+            return {
+                c_SUCCESS_RET_KEY: False,
+                c_CODE_RET_KEY: c_STUDY_DOES_NOT_EXIST
+            }
+        # Convert study name to study id
+        study_id = self.study_name_to_id(study_name)
+
+        # Get the share id for the email
+        ret_shares = self.do_get(command=self.api + "studies/"+str(study_id)+"/study_shares",
+                                 test=test)
+        share_id = None
+        for share in ret_shares[c_RESPONSE].json():
+            if share["email"]==email:
+                share_id = share['_id']['$oid']
+
+        permissions_info = {"study_id": study_id,
+                            "study_share":{"email":email,
+                                           "permission":access,
+                                           "deliver_emails":deliver_email}}
+        if share_id is None:
+            # Delete share
+            if(access==c_ACCESS_REMOVE):
+                return {
+                    c_SUCCESS_RET_KEY: False,
+                    c_CODE_RET_KEY: c_STUDY_DOES_NOT_EXIST
+                }
+            # Set shares for study given it does not exist
+            ret = self.do_post(command=self.api+"studies/"+str(study_id)+"/study_shares",
+                               values=permissions_info,
+                               test=test)
+            return(ret)
+        else:
+            # Delete share
+            if(access==c_ACCESS_REMOVE):
+                ret_delete = self.do_delete(command=self.api+"studies/"+str(study_id)+"/study_shares/"+share_id,
+                                            test=test)
+                return(ret_delete)
+            # Update shares for a study that has the shares
+            update_ret = self.do_patch(command=self.api+"studies/"+str(study_id)+"/study_shares/"+share_id,
+                                       values=permissions_info,
+                                       test=test)
+            return(update_ret)
+
+    def study_exists(self, study_name, test=False):
+        print("STUDY EXISTS?")
+        if self.studies is None:
+            ret = self.get_studies()
+            if not ret[c_SUCCESS_RET_KEY]:
+                return False
+                #### TODO throw exception
+        if test:
+            return(True)
+        return(study_name in self.studies)
+
+    def study_name_to_id(self, name, test=False):
+        print("STUDY NAME TO ID")
+        if test:
+            return("ID 1")
+        else:
+            return(self.name_to_id.get(name, None))
--- scripts/manage_study.py
+++ scripts/manage_study.py
@@ -2,8 +2,9 @@ import argparse
 import os
 import SCPAPI
 
-c_TOOL_STUDY = "Create_Study"
-c_TOOL_COORDINATES = "Upload_Coordinates"
+c_TOOL_STUDY = "create_study"
+c_TOOL_CLUSTER = "upload_cluster"
+c_TOOL_PERMISSION = "permission"
 
 def manageCallReturn(callReturn):
     # Print error code and describe code then exit if not sucess
@@ -40,20 +41,73 @@ subargs = args.add_subparsers()
 parser_create_studies = subargs.add_parser(c_TOOL_STUDY,
     help="Used to create studies. \""+args.prog+" "+c_TOOL_STUDY+" -h\" for more details")
 parser_create_studies.add_argument(
-    '--study_description', dest='studyDescription',
+    '--description', dest='studyDescription',
     default="Single Cell Genomics Study",
     help='Short description of the study.'
 )
 parser_create_studies.add_argument(
-    '--study_name', dest='studyName',
+    '--study_name', dest='studyName', required=True,
     help='The short name of the study.'
 )
 
-## Create file upload subparser
-#subargs = args.add_subparsers()
-#parser_upload_coordinates = subargs.add_parser(c_TOOL_COORDINATES,
-#    help="Used to upload coordinates files. \""+args.prog+" "+c_TOOL_COORDINATES+" -h\" for more details")
+## Permissions subparser
+parser_permissions = subargs.add_parser(c_TOOL_PERMISSION,
+    help="Used to change user permissions to studies. \""+args.prog+" "+c_TOOL_PERMISSION+" -h\" for more details")
+parser_permissions.add_argument(
+    '--email', dest='email', required=True,
+    default='Single Cell Genomics Study',
+    help='User email to update study permission.'
+)
+parser_permissions.add_argument(
+    '--study_name', dest='studyName', required=True,
+    help='The short name of the study.'
+)
+parser_permissions.add_argument(
+    '--access', dest='permission', choices=SCPAPI.c_PERMISSIONS, required=True,
+    help='The access to give the user. This can only be one of the following value:'+" ".join(SCPAPI.c_PERMISSIONS)
+)
 
+## Create cluster file upload subparser
+parser_upload_cluster = subargs.add_parser(c_TOOL_CLUSTER,
+    help="Used to upload cluster files. \""+args.prog+" "+c_TOOL_CLUSTER+" -h\" for more details")
+parser_upload_cluster.add_argument(
+    '--file', dest='clusterFile', required=True,
+    default=None,
+    help='Cluster file to load.'
+)
+parser_upload_cluster.add_argument(
+    '--study_name', dest='studyName', required=True,
+    default=None,
+    help='The name of the study to add the file.'
+)
+parser_upload_cluster.add_argument(
+    '--species', dest='species', required=True,
+    default=None,
+    help='The species from which the data is generated.'
+)
+parser_upload_cluster.add_argument(
+    '--genome', dest='genome', required=True,
+    default=None,
+    help='Genome assembly used to generate the data.'
+)
+parser_upload_cluster.add_argument(
+    '--x', dest='xLab',
+    default=None,
+    help='X axis label test.'
+)
+parser_upload_cluster.add_argument(
+    '--y', dest='yLab',
+    default=None,
+    help='y axis label test.'
+)
+parser_upload_cluster.add_argument(
+    '--z', dest='zLab',
+    default=None,
+    help='z axis label test.'
+)
+
+
+## TODO if no arguments are passed it logs in, stop logining in unless needed.
 parsed_args = args.parse_args()
 manage = SCPAPI.APIManager()
 manage.login(token=parsed_args.token,
@@ -75,6 +129,15 @@ if hasattr(parsed_args,"studyDescription") and not parsed_args.studyName is None
                               test=parsed_args.testing)
     manageCallReturn(ret)
 
+## Share with user
+if hasattr(parsed_args,"permission"):
+    print("Set PERMISSION")
+    ret = manage.set_permission(study_name=parsed_args.studyName,
+                                email=parsed_args.email,
+                                access=parsed_args.permission,
+                                test=parsed_args.testing)
+    manageCallReturn(ret)
+
 ## Validate and Upload coordinate file
 #if hasattr(parsed_args,"createStudy"):
 #    if(parsed_args.)
@@ -82,6 +145,7 @@ if hasattr(parsed_args,"studyDescription") and not parsed_args.studyName is None
 ## Validate and Upload metadata file
 
 ## Validate and Upload expression file
+##--
 
 ## Validate and Upload and Sort 10X files
 
