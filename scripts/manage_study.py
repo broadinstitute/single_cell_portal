07894e368211ad12ae9e22d01b65e1afd78f1ce3
TimothyTickle <timothyltickle@gmail.com>
2019-01-11 21:17:41 -0500

    Updates to make login only occur as needed.

--- scripts/manage_study.py
+++ scripts/manage_study.py
@@ -6,16 +6,26 @@ import Commandline
 import os
 import SCPAPI
 
-c_TOOL_STUDY = "create_study"
-c_TOOL_CLUSTER = "upload_cluster"
+c_TOOL_LIST_STUDY = "list-studies"
+c_TOOL_CLUSTER = "upload-cluster"
+c_TOOL_EXPRESSION = "upload-expression"
+c_TOOL_METADATA = "upload-metadata"
 c_TOOL_PERMISSION = "permission"
+c_TOOL_STUDY = "create-study"
 
 def manageCallReturn(callReturn):
     # Print error code and describe code then exit if not sucess
     print("Error Code = " + str(callReturn[SCPAPI.c_CODE_RET_KEY]))
-    print(SCPAPI.APIManager.describe_error_code(callReturn[SCPAPI.c_CODE_RET_KEY]))
+    print(SCPAPI.SCPAPIManager.describe_error_code(callReturn[SCPAPI.c_CODE_RET_KEY]))
     if not callReturn[SCPAPI.c_SUCCESS_RET_KEY]:
         exit(callReturn[SCPAPI.c_CODE_RET_KEY])
+
+def login(manager=None,test=False):
+    if manager is None:
+        manager=SCPAPI.SCPAPIManager()
+        manager.login(token=parsed_args.token,
+                      test=test)
+    return(manager)
 
 #def get_token_browser():
 #    "gcloud auth application-default login"
@@ -27,10 +37,6 @@ args = argparse.ArgumentParser(
     formatter_class=argparse.RawDescriptionHelpFormatter
 )
 args.add_argument(
-    '--list_studies', dest='listStudies', action='store_true',
-    help='List the studies editable by this user.'
-)
-args.add_argument(
     '--token', dest='token', default=None,
     help='Personal token after logging into Google (Oauth2). This token is not persisted after the finish of the script'
 )
@@ -38,10 +44,24 @@ args.add_argument(
     '--test', dest='testing', action='store_true',
     help='This will turn on testing mode which will walk through and log what will occur without performing the actions.'
 )
+args.add_argument(
+    '--no_validate', dest='validate',
+    action='store_false',
+    help='Do not check file locally before uploading.'
+)
 
 # Create tools (sub parser)
-## Create study subparser
 subargs = args.add_subparsers()
+
+## List studies
+parser_list_studies = subargs.add_parser(c_TOOL_LIST_STUDY,
+    help="Used to list studies. \""+args.prog+" "+c_TOOL_LIST_STUDY+" -h\" for more details")
+parser_list_studies.add_argument(
+    '--summary', dest='summarizeList', action='store_true',
+    help='Do not list but summarize only.'
+)
+
+## Create study subparser
 parser_create_studies = subargs.add_parser(c_TOOL_STUDY,
     help="Used to create studies. \""+args.prog+" "+c_TOOL_STUDY+" -h\" for more details")
 parser_create_studies.add_argument(
@@ -122,71 +142,96 @@ parser_upload_cluster.add_argument(
     default=None,
     help='z axis label test.'
 )
-parser_upload_cluster.add_argument(
-    '--no_validate', dest='validate',
-    action='store_false',
-    help='Check file locally before uploading.'
+
+## Create expression file upload subparser
+parser_upload_expression = subargs.add_parser(c_TOOL_EXPRESSION,
+    help="Used to upload expression files. \""+args.prog+" "+c_TOOL_EXPRESSION+" -h\" for more details")
+parser_upload_expression.add_argument(
+    '--file', dest='expressionFile', required=True,
+    help='Expression file to load.'
 )
 
+## Create metadata file upload subparser
+parser_upload_metadata = subargs.add_parser(c_TOOL_METADATA,
+    help="Used to upload metadata files. \""+args.prog+" "+c_TOOL_METADATA+" -h\" for more details")
+parser_upload_metadata.add_argument(
+    '--file', dest='metadataFile', required=True,
+    help='Metadata file to load.'
+)
 
-## TODO if no arguments are passed it logs in, stop logining in unless needed.
 parsed_args = args.parse_args()
-manage = SCPAPI.APIManager()
-manage.login(token=parsed_args.token,
-             test=parsed_args.testing)
+print("Args----")
 print(vars(parsed_args))
+print("-----Args")
+
+connection = None
+
 ## Handle list studies
-if parsed_args.listStudies:
+if hasattr(parsed_args,"summarizeList"):
     print("LIST STUDIES")
-    ret = manage.get_studies(test=parsed_args.testing)
+    connection = login(manager=connection, test=parsed_args.testing)
+    ret = connection.get_studies(test=parsed_args.testing)
     manageCallReturn(ret)
     print("There are "+str(len(ret[SCPAPI.c_STUDIES_RET_KEY]))+" studies to which you have access.")
-    print(os.linesep.join(ret[SCPAPI.c_STUDIES_RET_KEY]))
+    if not parsed_args.summarizeList:
+        print(os.linesep.join(ret[SCPAPI.c_STUDIES_RET_KEY]))
 
 ## Create new study
 if hasattr(parsed_args,"studyDescription") and not parsed_args.studyName is None:
     print("CREATE STUDY")
-    ret = manage.create_study(studyName=parsed_args.studyName,
-                              studyDescription=parsed_args.studyDescription,
-                              branding=parsed_args.branding,
-                              billing=parsed_args.billing,
-                              test=parsed_args.testing)
+    connection = login(manager=connection, test=parsed_args.testing)
+    ret = connection.create_study(studyName=parsed_args.studyName,
+                                  studyDescription=parsed_args.studyDescription,
+                                  branding=parsed_args.branding,
+                                  billing=parsed_args.billing,
+                                  test=parsed_args.testing)
     manageCallReturn(ret)
 
 ## Share with user
 if hasattr(parsed_args,"permission"):
     print("SET PERMISSION")
-    ret = manage.set_permission(studyName=parsed_args.studyName,
-                                email=parsed_args.email,
-                                access=parsed_args.permission,
-                                test=parsed_args.testing)
+    connection = login(manager=connection, test=parsed_args.testing)
+    ret = connection.set_permission(studyName=parsed_args.studyName,
+                                    email=parsed_args.email,
+                                    access=parsed_args.permission,
+                                    test=parsed_args.testing)
     manageCallReturn(ret)
 
 ## Validate files
-if hasattr(parsed_args,"clusterFile") and parsed_args.validate:
-    print("VALIDATE CLUSTER FILE")
-    if not parsed_args.testing:
-        valid_code = Commandline.Commandline().func_CMD(" ".join(["verify_portal_file.py",
-                                                                  "--coordinates_file",
-                                                                  parsed_args.clusterFile]))
+if parsed_args.validate:
+    print("VALIDATE FILES")
+    command = ["verify_portal_file.py"]
+
+    if hasattr(parsed_args,"clusterFile"):
+        command.extend(["--coordinates_file", parsed_args.clusterFile])
+    if hasattr(parsed_args,"expressionFile"):
+        command.extend(["--expression_files", parsed_args.expressionFile])
+    if hasattr(parsed_args,"metadataFile"):
+        command.extend(["--metadata_file", parsed_args.metadataFile])
+
+    if parsed_args.testing:
+        print("TESTING:: no command executed."+os.linesep+"Would have executed:"+os.linesep+" ".join(command))
+    else:
+        valid_code = Commandline.Commandline().func_CMD(" ".join(command))
         print(valid_code)
         if not valid_code:
-            print("There was an error validating the files, did not upload.")
+            print("There was an error validating the files, did not upload. Code="+str(valid_code))
             exit(valid_code)
 
 ## Validate and upload cluster file
 if hasattr(parsed_args,"clusterFile"):
     print("UPLOAD CLUSTER FILE")
-    ret = manage.upload_cluster(file=parsed_args.clusterFile,
-                                studyName=parsed_args.studyName,
-                                clusterName=parsed_args.clusterName,
-                                description=parsed_args.clusterDescription,
-                                species=parsed_args.species,
-                                genome=parsed_args.genome,
-                                x=parsed_args.xLab,
-                                y=parsed_args.yLab,
-                                z=parsed_args.zLab,
-                                test=parsed_args.testing)
+    connection = login(manager=connection, test=parsed_args.testing)
+    ret = connection.upload_cluster(file=parsed_args.clusterFile,
+                                    studyName=parsed_args.studyName,
+                                    clusterName=parsed_args.clusterName,
+                                    description=parsed_args.clusterDescription,
+                                    species=parsed_args.species,
+                                    genome=parsed_args.genome,
+                                    x=parsed_args.xLab,
+                                    y=parsed_args.yLab,
+                                    z=parsed_args.zLab,
+                                    test=parsed_args.testing)
     manageCallReturn(ret)
 
 ## Upload metadata file
