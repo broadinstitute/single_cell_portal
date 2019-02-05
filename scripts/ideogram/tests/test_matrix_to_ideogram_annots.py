a6eec140c4b54041ac8266d9fe879434d3629b1c
Eric Weitz <eweitz@broadinstitute.org>
2019-02-05 13:19:20 -0500

    Test conversion without references, document how to run

--- scripts/ideogram/tests/test_matrix_to_ideogram_annots.py
+++ scripts/ideogram/tests/test_matrix_to_ideogram_annots.py
@@ -1,3 +1,12 @@
+"""Unit tests for matrix_to_ideogram_annots.py
+
+To run, set up scripts per README, then:
+
+cd scripts/ideogram/tests
+python3 test_matrix_to_ideogram_annots.py
+
+"""
+
 import unittest
 from glob import glob
 import json
@@ -9,7 +18,7 @@ from matrix_to_ideogram_annots import create_parser, convert_matrix_and_write
 
 class MatrixToIdeogramAnnotsTestCase(unittest.TestCase):
 
-    def test_conversion(self):
+    def test_conversion_with_references(self):
         """Converter should handle default case with reference clusters
         """
         output_dir = 'output_infercnv_example/'
@@ -44,6 +53,39 @@ class MatrixToIdeogramAnnotsTestCase(unittest.TestCase):
         keys = annots['keys']
         expected_keys = [
             'name', 'start', 'length',
+            'malignant_MGH36', 'malignant_MGH53', 'malignant_97', 'malignant_93'
+        ]
+        self.assertEqual(keys, expected_keys)
+
+    def test_conversion_without_references(self):
+        """Converter should handle default case without reference clusters
+        I.e., without values for --ref-cluster-names
+        """
+        output_dir = 'output_infercnv_example/'
+        args = [
+            '--matrix-path', 'data/oligodendroglioma_expression_truncated.counts.matrix.txt',
+            '--gen-pos-file', 'data/gencode_v19_gene_pos_truncated_sorted.txt',
+            '--cluster-names', 'Observations',
+            '--cluster-paths', 'data/oligodendroglioma_annotations_downsampled.cluster.txt',
+            '--metadata-path', 'data/oligodendroglioma_annotations_downsampled.cluster.txt',
+            '--output-dir', output_dir
+        ]
+
+        args = create_parser().parse_args(args)
+        convert_matrix_and_write(args)
+
+        end_output_dir = output_dir + 'ideogram_exp_means/'
+
+        cluster_annots_file = end_output_dir + 'ideogram_exp_means__Observations--Sample--group--cluster.json'
+        with open(cluster_annots_file) as f:
+            annots = json.loads(f.read())
+
+        # Verify keys in Ideogram annotation data
+        # Note additional elements for microglia and oligodendrocyte clusters
+        keys = annots['keys']
+        expected_keys = [
+            'name', 'start', 'length',
+            'Microglia/Macrophage', 'Oligodendrocytes (non-malignant)',
             'malignant_MGH36', 'malignant_MGH53', 'malignant_97', 'malignant_93'
         ]
         self.assertEqual(keys, expected_keys)
