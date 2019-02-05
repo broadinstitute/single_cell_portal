0ebdd3a8a94d4a32a0428795cad1ac4b79f090b3
Eric Weitz <eweitz@broadinstitute.org>
2019-02-05 12:54:48 -0500

    Use mock data, test keys, comment

--- scripts/ideogram/tests/test_matrix_to_ideogram_annots.py
+++ scripts/ideogram/tests/test_matrix_to_ideogram_annots.py
@@ -1,5 +1,6 @@
 import unittest
 from glob import glob
+import json
 
 import sys
 sys.path.append('..')
@@ -9,27 +10,43 @@ from matrix_to_ideogram_annots import create_parser, convert_matrix_and_write
 class MatrixToIdeogramAnnotsTestCase(unittest.TestCase):
 
     def test_conversion(self):
+        """Converter should handle default case with reference clusters
+        """
         output_dir = 'output_infercnv_example/'
         args = [
-            '--matrix-path', '/Users/eweitz/inferCNV/example/oligodendroglioma_expression_downsampled.counts.matrix.txt',
-            '--gen-pos-file', '/Users/eweitz/single_cell_portal/infercnv_test_data/infercnv/infercnv_gencode_v19_gene_pos.txt',
+            '--matrix-path', 'data/oligodendroglioma_expression_truncated.counts.matrix.txt',
+            '--gen-pos-file', 'data/gencode_v19_gene_pos_truncated_sorted.txt',
             '--cluster-names', 'Observations',
-            '--cluster-paths', '/Users/eweitz/inferCNV/example/oligodendroglioma_annotations_downsampled.cluster.txt',
-            '--metadata-path', '/Users/eweitz/inferCNV/example/oligodendroglioma_annotations_downsampled.cluster.txt',
+            '--ref-cluster-names', 'Microglia/Macrophage', 'Oligodendrocytes (non-malignant)',
+            '--cluster-paths', 'data/oligodendroglioma_annotations_downsampled.cluster.txt',
+            '--metadata-path', 'data/oligodendroglioma_annotations_downsampled.cluster.txt',
             '--output-dir', output_dir
         ]
 
         args = create_parser().parse_args(args)
         convert_matrix_and_write(args)
 
-        files = glob(output_dir + 'ideogram_exp_means/*')
+        end_output_dir = output_dir + 'ideogram_exp_means/'
 
+        # Verify output file names
+        files = glob(end_output_dir + '*')
         expected_files = [
-            'output_infercnv_example/ideogram_exp_means/ideogram_exp_means__Observations--Sample--group--study.json',
-            'output_infercnv_example/ideogram_exp_means/ideogram_exp_means__Observations--Sample--group--cluster.json'
+            end_output_dir + 'ideogram_exp_means__Observations--Sample--group--study.json',
+            end_output_dir + 'ideogram_exp_means__Observations--Sample--group--cluster.json'
         ]
-
         self.assertEqual(files, expected_files)
+
+        cluster_annots_file = end_output_dir + 'ideogram_exp_means__Observations--Sample--group--cluster.json'
+        with open(cluster_annots_file) as f:
+            annots = json.loads(f.read())
+
+        # Verify keys in Ideogram annotation data
+        keys = annots['keys']
+        expected_keys = [
+            'name', 'start', 'length',
+            'malignant_MGH36', 'malignant_MGH53', 'malignant_97', 'malignant_93'
+        ]
+        self.assertEqual(keys, expected_keys)
 
 if __name__ == '__main__':
     unittest.main()
