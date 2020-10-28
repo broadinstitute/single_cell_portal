3f122e54032ae56d9cc5f5c21f11b56a9aa17762
jlchang <jlchang@broadinstitute.org>
2020-10-28 13:51:17 -0400

    increment for PyPI release

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name="single-cell-portal",
-    version="0.2.1",
+    version="0.2.2",
     description="Convenience scripts for Single Cell Portal",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/broadinstitute/single_cell_portal",
     author="Single Cell Portal team",
     author_email="scp-support@broadinstitute.zendesk.com",
-    install_requires=["pandas", "requests", "scp-ingest-pipeline==1.5.4"],
+    install_requires=["pandas", "requests", "scp-ingest-pipeline==1.7.0"],
     packages=find_packages(),
     entry_points={
         "console_scripts": ["manage-study=scripts.manage_study:main"],
